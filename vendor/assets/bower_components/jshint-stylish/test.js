6f821f455432df5e7ba0cb6bfbe4323974971ca9
Matthew Mazzuca <matthew.mazzuca@mail.utoronto.ca>
2015-06-23 15:52:04 -0400

    added appropriate bower components

---
+++ vendor/assets/bower_components/jshint-stylish/test.js
@@ -0,0 +1,27 @@
+'use strict';
+var assert = require('assert');
+var chalk = require('chalk');
+var jshint = require('jshint/src/cli');
+var stylish = require('./');
+
+it('should be used by JSHint', function () {
+	var ret = false;
+	var _log = process.stdout.write.bind(process.stdout);
+
+	process.stdout.write = function (str) {
+		_log(str);
+
+		if (/'foo' is defined/ig.test(chalk.stripColor(str || ''))) {
+			ret = true;
+		}
+	};
+
+	jshint.run({
+		args: ['fixture.js'],
+		reporter: stylish.reporter
+	});
+
+	process.stdout.write = _log;
+
+	assert(ret);
+});
